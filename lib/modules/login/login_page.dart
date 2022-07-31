b48a4a1ee7e45173b5757ef30621a90bcdc2e4d6
MatheusFontenele <matheus.fontenele@icloud.com>
2022-07-31 16:50:02 -0300

    feat: create login page

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SizedBox(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              width: size.width,
+              height: size.height * 0.36,
+            ),
+            Positioned(
+              top: size.height * 0.08,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImage.person,
+                width: 208,
+                height: 373,
+              ),
+            ),
+            Positioned(
+              bottom: size.height * .08,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  Image.asset(AppImage.codeBarSmall),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 70, right: 70, top: 20),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      style: AppTextStyles.titleHome,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 50, right: 50, top: 50),
+                    child: SocialButton(
+                      onTap: () {
+                        print('hola');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialButton extends StatelessWidget {
+  final VoidCallback onTap;
+  const SocialButton({
+    Key? key,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+            color: AppColors.shape,
+            borderRadius: BorderRadius.circular(8),
+            border: const Border.fromBorderSide(
+                BorderSide(color: AppColors.stroke))),
+        child: Row(children: [
+          Expanded(
+              flex: 1,
+              child: Container(
+                decoration: const BoxDecoration(
+                    border: Border(
+                        right: BorderSide(color: AppColors.stroke, width: 2))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(AppImage.googleIcon),
+                  ],
+                ),
+              )),
+          Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Entrar com google',
+                    style: AppTextStyles.buttonGray,
+                  ),
+                ],
+              ))
+        ]),
+      ),
+    );
+  }
+}
