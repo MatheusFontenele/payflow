1d4a0eb86dd805cece25468effaca525ed5a9e89
MatheusFontenele <matheus.fontenele@icloud.com>
2022-07-31 11:42:31 -0300

    refactor: add correct images

--- lib/shared/themes/app_images.dart
+++ lib/shared/themes/app_images.dart
@@ -1,6 +1,7 @@
 class AppImage {
-  static const union = "assets/images/union.png";
-  static const unionSmall = "assets/images/union-small.png";
+  static const union = "assets/images/Union.png";
+  static const codeBarSmall = "assets/images/code-bar-small.png";
+  static const codeBar = "assets/images/code-bar.png";
   static const logo = "assets/images/Logotipo.png";
   static const person = "assets/images/person.png";
   static const googleIcon = "assets/images/google-icon.png";
