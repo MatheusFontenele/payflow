f0ebb8d1ba88001278558b8a649d6d54124888dd
MatheusFontenele <matheus.fontenele@icloud.com>
2022-07-31 11:21:29 -0300

    feat: create app theme

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,7 @@
+class AppImage {
+  static const union = "assets/images/union.png";
+  static const unionSmall = "assets/images/union-small.png";
+  static const logo = "assets/images/Logotipo.png";
+  static const person = "assets/images/person.png";
+  static const googleIcon = "assets/images/google-icon.png";
+}
