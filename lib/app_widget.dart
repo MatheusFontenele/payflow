64bdcb1259401a9bb0650f82638b9ed39539a92e
MatheusFontenele <matheus.fontenele@icloud.com>
2024-08-06 23:50:01 -0300

    odeio flutter

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+// import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  AppWidget() {
+  AppWidget({super.key}) {
     SystemChrome.setPreferredOrientations(
       [
         DeviceOrientation.portraitDown,
@@ -30,7 +30,7 @@ class AppWidget extends StatelessWidget {
         "/splash": (context) => const SplashPage(),
         "/home": (context) => const HomePage(),
         "/login": (context) => const LoginPage(),
-        "/barcode_scanner": (context) => const BarcodeScannerPage(),
+        // "/barcode_scanner": (context) => const BarcodeScannerPage(),
       },
     );
   }
